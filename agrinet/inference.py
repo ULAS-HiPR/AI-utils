eef522bb217d0ec280b6986c3224a593eba59bdf
Adam Byrne <adamrbyrne@gmail.com>
2024-02-04 15:07:15 +0000

    status: fully working

--- agrinet/inference.py
+++ agrinet/inference.py
@@ -1,57 +1,57 @@
 import argparse
 
-import tensorflow as tf
+import numpy as np
+
 from utils.LogManager import LogManager
 from utils.Model import Generator
+from utils.DataLoader import load_single, save_result
+from utils.EnviroIndices import parse_indices
 
 
 def main(args):
     logger = LogManager.get_logger("AGRINET INFERENCE")
-    logger.info("Loading generator...")
+    logger.info("Warming up...")
 
     generator = Generator()
-
     try:
-        saved_model = tf.saved_model.load(args.weights)
-        generator.load_weights(saved_model)
+        generator.load_weights(f"{args.exp}/generator_weights")
+        logger.debug("Model loaded")
+
     except Exception as e:
         logger.critical("Error while loading weights: {}".format(e))
 
-    logger.info("Model loaded")
-
-    logger.info("Loading input image...")
     input = None
 
     try:
-        input = tf.io.read_file(args.input)
+        input = load_single(args.input)
+        logger.debug("Input loaded")
     except Exception as e:
-        logger.critical("Error while reading input image: {}".format(e))
+        logger.critical("Error while loading input: {}".format(e))
 
-    if input is None:
-        logger.critical("Input image is empty")
-    else:
-        logger.info("Input image loaded")
+    input = np.expand_dims(input, 0)
+    prediction = generator(input, training=False)[0]
+    save_result(input[0], prediction, f"{args.exp}/results/output.jpg")
+    logger.info("Output saved")
 
-    input = tf.image.decode_jpeg(input)
-    input = tf.image.resize(input, [256, 256])
-    input = (input / 127.5) - 1  # Normalize the images to [-1, 1]
-    input = tf.expand_dims(input, axis=0)
-    input = tf.cast(input, tf.float32)
+    ndvi, ndwi, ndvi_regions, ndwi_regions = parse_indices(
+        input[0], prediction, regions=True
+    )
 
-    output = generator(input, training=True)
-    output = tf.cast(output[0], tf.uint8)
-    output = tf.image.encode_jpeg(output)
+    ndvi = np.stack([np.zeros_like(ndvi), ndvi, np.zeros_like(ndvi)], axis=-1)
+    ndwi = np.stack([np.zeros_like(ndwi), np.zeros_like(ndwi), ndwi], axis=-1)
+    save_result(ndvi, ndwi, f"{args.exp}/results/indices.jpg")
+    logger.info("Indices saved")
 
-    try:
-        tf.io.write_file("results/output.jpg", output)
-    except Exception as e:
-        logger.critical("Error while saving utput image: {}".format(e))
+    ndvi_regions = ndvi_regions * 255
+    ndwi_regions = ndwi_regions * 255
+    save_result(ndvi_regions, ndwi_regions, f"{args.exp}/results/regions.jpg")
+    logger.info("Regions saved")
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weights", type=str, required=True)
-    parser.add_argument("--input", type=str, required=True)
+    parser.add_argument("--exp", type=str, required=True, help="Experiment name")
+    parser.add_argument("--input", type=str, required=True, help="Input image path")
     args = parser.parse_args()
     return args
 
