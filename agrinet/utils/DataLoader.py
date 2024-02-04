6c7c5c2fb8aedcbc62a93823a7d35a2b3b910999
Adam Byrne <adamrbyrne@gmail.com>
2024-02-04 15:04:10 +0000

    allow saving grayscales

--- agrinet/utils/DataLoader.py
+++ agrinet/utils/DataLoader.py
@@ -27,26 +27,38 @@ def load_single(image_file):
     image = tf.io.decode_jpeg(image)
 
     image = tf.cast(image, tf.float32)
-    image = tf.image.resize(image, [256, 256])
+    image = resize(image, None, IMG_HEIGHT, IMG_WIDTH)
+    image = normalize(image, None)
 
     return image
 
 
-def save_single(image, path):
-    image = tf.image.resize(image, [256, 256])
-    image = tf.image.convert_image_dtype(image, tf.uint8)
+def save_result(input_img, output_img, path):
+    input_img = tf.image.convert_image_dtype(input_img, tf.uint8)
+    output_img = tf.image.convert_image_dtype(output_img, tf.uint8)
+    image = tf.concat([input_img, output_img], axis=1)  # side by side
+
+    if image.shape == (256, 512):  # grayscale image
+        image = tf.expand_dims(image, axis=-1)
+        image = tf.image.grayscale_to_rgb(image)
+
     image = tf.image.encode_jpeg(image)
+    tf.io.write_file(path, image)
 
 
 def resize(input_image, real_image, height, width):
     input_image = tf.image.resize(
         input_image, [height, width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
     )
-    real_image = tf.image.resize(
-        real_image, [height, width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
-    )
 
-    return input_image, real_image
+    if real_image is not None:
+        real_image = tf.image.resize(
+            real_image, [height, width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
+        )
+
+        return input_image, real_image
+
+    return input_image
 
 
 def random_crop(input_image, real_image):
@@ -61,9 +73,12 @@ def random_crop(input_image, real_image):
 # Normalizing the images to [-1, 1]
 def normalize(input_image, real_image):
     input_image = (input_image / 127.5) - 1
-    real_image = (real_image / 127.5) - 1
 
-    return input_image, real_image
+    if real_image is not None:
+        real_image = (real_image / 127.5) - 1
+        return input_image, real_image
+
+    return input_image
 
 
 @tf.function()
